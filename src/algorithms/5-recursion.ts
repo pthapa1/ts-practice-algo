a2852cc83fa72a30f3d3abb358bfd3bb971ae552
Pratik Thapa <pratik@tldrnotes.net>
2023-07-06 15:16:58 -0600

    basic recursion example

--- src/algorithms/5-recursion.ts
+++ src/algorithms/5-recursion.ts
@@ -1,7 +1,13 @@
-/*
-[
- '#####E#', 
- '#     #', 
- '#S#####'
-]
-*/
+// program to count down numbers to 1
+function countDown(num: number) {
+  // display the number
+  console.log(num);
+  // decrease the number value
+  const newNumber = num - 1;
+  // base case
+  if (newNumber > 0) {
+    countDown(newNumber);
+  }
+}
+
+countDown(4);
